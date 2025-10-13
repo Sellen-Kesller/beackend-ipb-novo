e569ed4b9782352bf9575e9b5d3fba47994e2d4d
sellenkesller <sellen-o-play@hotmail.com>
2025-10-13 02:37:02 -0300

    erro login

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-const User = require('./models/User'); // ✅ IMPORTE O MODELO DE USUÁRIO
+const User = require('./models/User'); 
 
 require('dotenv').config();
 
